1250c1fbe15570c0566effdb30dd90e26962b9cc
Lucas Damaceno <ldamaceno@lendingclub.com>
2023-03-14 16:12:55 -0300

    Add maven deploy plugins

--- src/main/kotlin/org/kmapper/testClasses/OriginClass.kt
+++ src/main/kotlin/org/kmapper/testClasses/OriginClass.kt
@@ -1,3 +1,12 @@
 package org.kmapper.testClasses
 
-data class OriginClass(val int:Int, val string:String, val double: Double, val short: Short, val byte: Byte)
+import org.kmapper.KMappedField
+
+data class OriginClass(
+@KMappedField(destinationField = "testInt")
+val int:Int,
+@KMappedField(destinationField = "testString")
+val string:String,
+val double: Double,
+val short: Short,
+val byte: Byte)
